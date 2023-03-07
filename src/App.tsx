d926792d9fc575114b3c51f9fdee20d6936c840d
Eduardo Mendes <eduardo.mendes@sensedia.com>
2023-03-07 15:17:33 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FaInstagram } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Social Media
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      < ul>
+      <li><a href="/"><i className="fa-brands"> <FaInstagram/></i></a></li>
+      <li><a href="/"><i className="fa-brands"> <FaInstagram/></i></a></li>
+      <li><a href="/"><i className="fa-brands"> <FaInstagram/></i></a></li>
+       </ul>
       </header>
     </div>
   );
